0761cbd05b59e5cfd5a654f640e7124a6cebfe02
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-21 13:19:20 -0700

    Create Index view and Location detail view

--- homesapp/urls.py
+++ homesapp/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from homesapp import views
+from django.urls import path, re_path
+from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('homesapp/', include('homesapp.urls'))
+app_name = 'homesapp'
+
+urlpatterns = [    
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.LocationView.as_view(), name = 'property')
 ]
--- homesapp/views.py
+++ homesapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Location
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'homesapp/index.html'
+
+    def get_queryset(self):
+        return Location.objects.all()
+
+class LocationView(generic.DetailView):
+    template_name = 'homesapp/location_detail.html'
+    model = Location
